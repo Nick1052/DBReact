bd23249a3ea95985363f1f03e81a4a858cdfbb7a
Nick <nickgr1052@gmail.com>
2023-04-17 22:40:38 -0400

    Push working code.

--- src/components/CustomPopUp.js
+++ src/components/CustomPopUp.js
@@ -2,11 +2,10 @@ import React, {useState} from "react";
 
 
 function Popup(props) {
-    
     return (props.trigger) ? (
         <div className="popup">
             <div className="pop-up-inner">
-                <button className="popup-close-btn">Submit</button>
+                <button className="popup-close-btn" onClick={() => props.setTrigger(false)}>Submit</button>
                 {props.children}
             </div>
         </div>
